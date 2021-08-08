06c077e4c666086935e2939b750e7fc8ef246380
JS Caughlin <jcaughlin@gmail.com>
2021-08-08 14:29:53 -0500

    Created a seeds.db file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+application_list = [
+  ["Google",	"Search Engine", "Red", true,"http://www.google.com"],
+  ["Wisc", "UW homepage", "Blue", false	http://www.wisc.edu],
+  ["GLBRC",	"Great Lakes Bioenergy Research Center", "Yellow", true, "http://www.glbrc.org"],
+  ["WEI",	"Wisconsin Energy Institute",	"Green", false, "https://energy.wisc.edu/]",  
+  ["Twitter",	"Twitter", "Purple", false,	"https://twitter.com/"] 
+]
+
+application_list.each do |name,description,color,default_status,link|
+  Application.create(name: name,description: description,color:color,default_status: default_status,link: link)
+end
