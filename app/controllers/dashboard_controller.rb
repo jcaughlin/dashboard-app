a378bc755496c321d20b5a98846bc776724d0c6e
JS Caughlin <jcaughlin@gmail.com>
2021-08-10 16:19:48 -0500

    Added capability to add apps to user profile.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,20 @@
 class DashboardController < ApplicationController
   def main
-    # @disabled_apps = Application.where("default_status = false")
+   
     @user_active_apps = Current.user.applications
 
-  
+    # TABLE NAMES
+    # applications
+    # users
+    # user_applications COLUMN NAMES; user_id, application_id
 
-    # @enabled_apps = Application.where("default_status = true")
+    @disabled_appications = Application.where("id NOT IN (SELECT user_applications.application_id FROM user_applications WHERE (user_applications.user_id = :user_id))", user_id: Current.user.id)
   end
 
   def add
-  
+    user = Current.user
+    app = Application.find_by(id: params[:id])
+    user.applications << app
+    redirect_to dashboard_path(user.id), notice: "Dashboard Successfully Updated!"
   end
 end
